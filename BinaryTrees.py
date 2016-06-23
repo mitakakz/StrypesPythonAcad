84ce947c286728d7906017e0b7b685fbe6ea4282
Dimitar Dimitrov <mitaka_kz@abv.bg>
2016-06-23 09:45:57 +0300

    New impl of DoubleLinkedLists & Basic struct of Bin Trees

---
+++ BinaryTrees.py
@@ -0,0 +1,104 @@
+from collections import deque
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, node):
+        if not isinstance(node, Node):
+            raise ValueError('node should be Node')
+
+        self.children.append(node)
+
+
+class Tree:
+    def __init__(self, *, root):
+        """
+            When we are creating a new tree, we must always have a root element.
+            For example:
+            tree = Tree(root=5)
+        """
+
+        self.__root = Node(root)
+        self.__found_by_value = None  # the Node containing the searched value
+
+
+    def add(self, *, value, parent):
+        """
+            When we are adding new element to our tree, we must specify the parent:
+            tree = Tree(root=5)
+            tree.add(4, parent=5)
+            tree.add(3, parent=5)
+            tree.add(2, parent=4)
+
+            This will make the following tree:
+
+                5
+               / \
+              4   3
+             /
+            2
+        """
+        elem = Node(value)
+
+        self.search(parent, self.__root)
+
+        self.__found_by_value.add_child(elem)
+        self.__found_by_value = None
+
+    def search(self, x, vertex):
+        if x == vertex.value:
+            self.__found_by_value =  vertex
+        print(vertex.value)
+        for child in vertex.children:
+            self.search(x, child)
+
+    def find(self, x):
+        """
+            Returns True or False if Node with value x is present in the tree
+        """
+        self.__found_by_value = None
+        self.search(x, self.__root)
+        if self.__found_by_value != None:
+            return True
+        else:
+            return False
+
+    def bfs_from_root(self):
+        """
+            Makes a Breadth-First-Search Algorithm from root
+            Returns a list of tuples in the following format:
+            [(tree_level, (node1_on_that_level, node2_on_that_level, ...)),
+             (tree_level + 1, (node1_on_that_level, node2_on_that_level, ...))]
+
+            If we take the tree from the add example, the result will look like that:
+
+            [(1, (5, )),
+             (2, (4, 3)),
+             (3, (2, ))]
+
+             We count our levels from 1.
+        """
+        pipe = deque()
+        pipe.append(self.__root)
+        result = []
+        elems_per_level = ()
+        level = 1
+
+        while True:
+            for elem in pipe:
+                elems_per_level += elems_per_level+(elem.value,)
+            result += [(level,elems_per_level)]
+
+
+
+
+
+
+tree = Tree(root=5)
+tree.add(value=4,parent=5)
+tree.add(value=3,parent=5)
+print("__________________________________")
+print(tree.find(4))
+tree.bfs_from_root()
--- DataStructures.py
+++ DataStructures.py
@@ -41,7 +41,7 @@ class Vector:
         return self.__current_capacity
 
 
-class Node:
+class Linked_list:
     """From a linked list"""
 
     def __init__(self, value, next_node=None):
@@ -54,53 +54,188 @@ class Node:
     def has_next(self):
         return self.next_node is not None
 
+class Node:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
 
-class DoubleLinkedList(Node):
+        if next is not None and\
+                not isinstance(next, Node):
+            raise TypeError('next should be instance of Node')
 
-    """All defs should have 0(1) complexity"""
-    def __init__(self,value, next_node=None, prev_node=None):
-        self.prev = prev_node
+        if prev is not None and\
+                not isinstance(prev, Node):
+            raise TypeError('prev should be instance of Node')
 
-        if (next_node is not None or prev_node is not None) and \
-                (not isinstance(next_node, Node) or not isinstance(next_node, DoubleLinkedList)):
-            raise TypeError("next node should be an instance of Node or DoubleLinkedList")
+        self.prev = prev
+        self.next = next
 
-        super().__init__(value,next_node)
+    def has_prev(self):
+        return self.prev is not None
+
+    def has_next(self):
+        return self.next is not None
 
     def __str__(self):
-        return str(self.value)
+        return "{}<-{}->{}".format(self.prev, self.value, self.next)
 
-    def add_first(self, head):
-        self.next_node = head
-        head.prev = self
 
-    def add_last(self, tail):
-        self.prev = tail
-        tail.next_node = self
+class DoublyLinkedList():
+    def __init__(self):
+        self.head_point = None
+        self.tail_point = None
+        self.count = 0
 
+
+    """ O(1) - Returns a Node object that represents the head """
+    def get_head(self):
+        return self.head_point
+
+
+    """ O(1) - Returns a Node object that represents the head """
+    def get_tail(self):
+        return  self.tail_point
+
+    """ O(1) - Returns the value of the first element """
+    def get_first(self):
+        return self.head_point.value
+
+    """ O(1) - Returns the value of the last element """
+    def get_last(self):
+        if self.tail_point != None:
+            return self.tail_point.value
+        else:
+            return None
+
+    """ O(1) """
+    def add_first(self, x):
+        elem = Node(x)
+        if self.head_point != None:
+            self.head_point.prev = elem
+            elem.next = self.head_point
+        self.head_point = elem
+        if self.tail_point == None:
+            self.tail_point = elem
+        self.count += 1
+
+
+    """ O(1) """
+    def add_last(self, x):
+        elem = Node(x)
+        if self.tail_point != None:
+            self.tail_point.next = elem
+            elem.prev = self.tail_point
+        self.tail_point = elem
+        if self.head_point == None:
+            self.head_point = elem
+        self.count += 1
+
+    """ O(1) """
     def remove_first(self):
-        self.next_node.prev = None
+        if self.head_point == self.tail_point:
+            self.tail_point = None
+        if self.head_point != None:
+            val = self.head_point.value
+            self.head_point = self.head_point.next
+        if self.head_point != None:
+            self.head_point.prev = None
+        self.count -= 1
+        return val
 
-    def remove_last(self,x):
-        self.prev.next_node = None
+    """ O(1) """
+    def remove_last(self):
+        if self.head_point == self.tail_point:
+            self.head_point = None
 
-    def print_list(self):
-        obj = self
-        while True :
+        if self.tail_point != None:
+            val = self.tail_point.value
+            self.tail_point = self.tail_point.prev
 
-            print(obj)
-            if obj.next_node == None:
-                break
-            obj = obj.next_node
+        if self.tail_point != None:
+            self.tail_point.next = None
+        if self.count > 0:
+            self.count -= 1
+            return val
+        else: return None
+
+    """ O(1) """
+    def __len__(self):
+        return self.count
+
+    """ O(n) - Called when we do list(items) """
+    def __iter__(self):
+        result = []
+        node = self.head_point
+        while node != None:
+            result.append(node.value)
+            node = node.next
+        return iter(result)
+
+class Stack():
+
+    def __init__(self):
+        self.__dll = DoublyLinkedList()
+        self.result = True
+    def push(self, x):
+        self.__dll.add_last(x)
+        self.result = True
+
+    def pop(self):
+        val = self.__dll.get_last()
+        self.__dll.remove_last()
+        if val != None:
+            return val
 
 
-a = DoubleLinkedList(2)
-c = DoubleLinkedList(4)
-b = DoubleLinkedList(3,c,a)
-a.add_first(b)
-h = DoubleLinkedList(1)
-h.add_first(a)
-c.add_last(b)
+    def get_depth(self):
+        return self.__dll.count
 
 
-h.print_list()
+
+open = Stack()
+close = Stack()
+
+for el in "((()))":
+    if el == "(":
+        open.push(1)
+    if el == ")":
+        close.push(1)
+print(open.get_depth(), close.get_depth())
+
+if open.get_depth() == close.get_depth():
+    print("True")
+else:
+    print("False")
+
+"""
+for el in "(()))":
+    if el == "(":
+         print(braces.gqet_depth())
+    if el == ")":
+        braces.pop()
+        print(braces.get_depth())
+print(braces.get_depth())
+if braces.get_depth() ==0 :
+    print("True")
+else:
+    print("False")
+
+
+"""
+
+
+
+"""
+items = DoublyLinkedList()
+items.add_first(5)
+items.add_first(6)
+
+assert items.get_first()==6
+assert items.get_last()==5
+assert len(items) == 2
+assert list(items) == [6,5]
+
+assert items.remove_first() == 6
+assert len(items) == 1
+assert items.remove_first() == 5
+"""
+
